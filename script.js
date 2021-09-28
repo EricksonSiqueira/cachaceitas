be22b36e2c280a758bb7ca44b9704ff4c7598b76
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-09-28 19:27:31 -0300

    Pegando drink pelo id

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const getDrinkByName = async (drink) => {
   const API_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
   const res = await fetch(`${API_URL}${drink}`);
   const data = await res.json();
-  
+
   return data;
 };
 
@@ -25,20 +25,46 @@ const getRandomDrink = async () => {
   return data;
 };
 
-const createDrink = async ({ strDrinkThumb, strDrink }) => {
-    const section = document.querySelector('.drinks-section');
+const getDrinkById = async (id) => {
+  const API_URL = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+  const res = await fetch(`${API_URL}${id}`);
+  const data = await res.json();
+  return (data);
+}
 
-    const div = document.createElement('div');
-    div.className = 'drink';
-    const img = document.createElement('img');
-    img.src = strDrinkThumb;
-    img.className = 'drink-img';
-    const span = document.createElement('span');
-    span.innerText = strDrink;
+const getIngredientsArr = (drink) => {
+  const ingredients = [];
+  for (let i = 1; i <= 15; i += 1) {
+    const ingredient = drink.drinks[0][`strIngredient${i}`];
+    if (ingredient !== null) ingredients.push(ingredient);
+  }
+  return ingredients;
+}
 
-    div.appendChild(img);
-    div.appendChild(span);
-    section.appendChild(div);
+const getDrinkID = async ({ target }) => {
+  const div = document.querySelector('.drink');
+  if (target !== div) {
+    return getDrinkById(target.closest('.drink').id);
+  } else {
+    return getDrinkById(target.id);
+  }
+};
+
+const createDrink = async ({ strDrinkThumb, strDrink, idDrink }) => {
+  const section = document.querySelector('.drinks-section');
+
+  const div = document.createElement('div');
+  div.className = 'drink';
+  div.id = idDrink;
+  div.addEventListener('click', getDrinkID);
+  const img = document.createElement('img');
+  img.src = strDrinkThumb;
+  img.className = 'drink-img';
+  const span = document.createElement('span');
+  span.innerText = strDrink;
+  div.appendChild(img);
+  div.appendChild(span);
+  section.appendChild(div);
 }
 
 const clearDrinksSection = () => {
@@ -52,11 +78,11 @@ const renderClaudiaoError = () => {
 
 const renderDrink = async (searchType, search) => {
   try {
-    if(searchType === 'name'){
+    if (searchType === 'name') {
       clearDrinksSection();
       const newDrink = await getDrinkByName(search);
       newDrink.drinks.forEach((drink) => createDrink(drink));
-  
+
     } else if (searchType === 'ingredient') {
       clearDrinksSection();
       const newDrink = await getDrinkByIngredient(search);
@@ -84,5 +110,5 @@ randomButton.addEventListener('click', async (event) => {
 })
 
 window.onload = () => {
-    renderDrink('name', '');
+  renderDrink('name', '');
 };
